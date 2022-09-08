54694c3d37a59155cbd35c09122916182631605c
supergirl <supergirl@anniecannons.com>
2022-09-07 23:45:31 -0700

    updated the fetchUrls and movieTrailers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
-import Banner from './Banner'
-import Nav from './Nav';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
@@ -13,19 +13,33 @@ function App() {
       <Nav />
       {/* Banner */}
       <Banner />
-      <Row 
-        title="NETFLIX ORIGINALS"  
-        fetchUrl={requests.fetchNetflixOriginals} 
-        isLargeRow = {true}
+      <Row
+        title={"NETFLIX ORIGINALS"}
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
       />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row title={"Trending Now"} fetchUrl={requests.fetchTrending} />
+      <Row title={"History Movies"} fetchUrl={requests.fetchHistoryMovies} />
+      <Row
+        title={"Animation Movies"}
+        fetchUrl={requests.fetchAnimationMovies}
+      />
+      <Row title={"Fantasy Movies"} fetchUrl={requests.fetchFantasyMovies} />
+      <Row title={"Romance Movies"} fetchUrl={requests.fetchRomanceMovies} />
     </div>
+    //   <Row
+    //     title="NETFLIX ORIGINALS"
+    //     fetchUrl={requests.fetchNetflixOriginals}
+    //     isLargeRow = {true}
+    //   />
+    //   <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    //   <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+    //   <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+    //   <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+    //   <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+    //   <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+    //   <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    // </div>
   );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,38 +1,35 @@
-import React, {useEffect, useState} from 'react'
-import './Nav.css';
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
 
 function Nav() {
-    const [show, handleShow] = useState(false);
+  const [show, handleShow] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                handleShow(true);
-            } else handleShow(false);
-        });
-        return () => {
-            window.removeEventListener("scroll");
-        };
-        
-    }, [])
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+        alt="Netflix Logo"
+      />
 
-    return (
-        <div className={`nav ${show && "nav_black"}`}>
-            <img
-                className="nav_logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
-                alt="Netflix Logo"
-            />
-
-            <img
-                className="nav_avatar"
-                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-                alt="Netflix Avatar"
-            />
-            
-        </div>
-    )
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Avatar"
+      />
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
